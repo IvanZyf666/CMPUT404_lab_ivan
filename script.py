c039bec3bc330fbcfe3a00d0f8712d74717f0068
Ivan Zhang <yzhang24@ualberta.ca>
2021-09-08 17:34:44 -0600

    update

--- script.py
+++ script.py
@@ -5,4 +5,7 @@ print("requests version:", requests.__version__)
 r = requests.get('https://www.google.com/') # get google homepage 
 
 print(r.text)
-# what should I print?
+
+new_r = requests.get('https://raw.githubusercontent.com/IvanZyf666/CMPUT404_lab_ivan/main/script.py')
+
+print(new_r.text)
