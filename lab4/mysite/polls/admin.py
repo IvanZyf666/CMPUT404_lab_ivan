639b7d4b12355965442d90141e1938874416af23
Ivan <yzhang24@ualberta.ca>
2021-10-08 11:45:33 -0600

    Generate Pelican site

--- lab4/mysite/polls/admin.py
+++
@@ -1,9 +0,0 @@
-
-from django.contrib import admin
-
-from .models import Choice, Question, Department
-
-admin.site.site_header = 'My First Django Website Admin'
-admin.site.register(Choice)
-admin.site.register(Question)
-admin.site.register(Department)
